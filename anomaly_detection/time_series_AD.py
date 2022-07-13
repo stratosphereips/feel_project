ddbbc7842603cec85d01966f704fd1b24c041663
mari <mrigaki@gmail.com>
2022-07-13 15:13:17 +0200

    Remove matplotlib dependency

--- anomaly_detection/time_series_AD.py
+++ anomaly_detection/time_series_AD.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
-from matplotlib import pyplot as plt
 
 master_url_root = "https://raw.githubusercontent.com/numenta/NAB/master/data/"
 
