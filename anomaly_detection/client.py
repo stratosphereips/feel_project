Fixed bug with TPR calculatuon in the server and issues with scaling

anomaly_detection/client.py
@@ -53,6 +53,7 @@ class ADClient(fl.client.NumPyClient):
         X_train = scale_data(self.X_train, self.X_min, self.X_max)
         X_train, X_val = train_test_split(X_train, test_size=0.2, random_state=8181)
 
+
         # Train the model using hyperparameters from config
         history = self.model.fit(
             X_train,
@@ -66,8 +67,17 @@ class ADClient(fl.client.NumPyClient):
         val_loss = history.history["val_loss"][0]
 
         if np.isnan(loss) or np.isnan(val_loss):
-            exit()
-            # return None,  0, {}
+            print("[+] HERE!", np.isnan(loss), np.isnan(val_loss))
+            print("[+] HERE!", history.history['loss'])
+            # exit()
+            # Return the previous parameters if the loss exploded
+            return parameters,  0, {
+                        "loss": loss,
+                        "val_loss": val_loss,
+                        "threshold": float(self.threshold),
+                        "X_min": serialize_array(self.X_min),
+                        "X_max": serialize_array(self.X_max)
+                        }
 
         # Calculate the threshold based on the local tarining data
         rec = self.model.predict(X_val)
@@ -108,7 +118,7 @@ class ADClient(fl.client.NumPyClient):
         X_test_ben_ = scale_data(self.X_test_ben, self.X_min, self.X_max)
 
         # Evaluate global model parameters on the local test data and return results
-        loss = self.model.evaluate(X_test_ben_, X_test_ben_, 32, steps=None)
+        loss = self.model.evaluate(X_test_ben_, X_test_ben_, 64, steps=None)
         num_examples_test = len(X_test_ben_)
 
         rec_ben = self.model.predict(X_test_ben_)
@@ -153,6 +163,7 @@ class ADClient(fl.client.NumPyClient):
         tpr = tp / num_malware
         fpr = fp / num_examples_test
 
+
         return loss, int(num_examples_test+num_malware), {
                             "anomalies_ben": int(anomalies_ben), 
                             "anomalies_mal": int(anomalies_mal),
@@ -194,6 +205,7 @@ def load_partition(day: int, client_id: int):
 
     # num_samples = x_train.shape[0] // 5
     print(f"[+] Num train samples for client{client_id}: {X_train.shape[0]}")
+    print(f"[+] Num of features for client{client_id}: {X_train.shape[1]}")
     return X_train, X_test_ben, X_test_mal
     # return x_train[idx * num_samples : (idx + 1) * num_samples], x_test[idx * num_samples : (idx + 1) * num_samples]
 

anomaly_detection/server.py
@@ -106,7 +106,7 @@ def main() -> None:
     
     day = args.day
 
-    # TODO: initialize the model parameters from the precious day
+    # TODO: initialize the model parameters from the previous day
     model = get_model()
 
     # Create custom strategy that aggregates client metrics
@@ -114,7 +114,7 @@ def main() -> None:
         fraction_fit=1.0,
         fraction_evaluate=1.0,
         min_fit_clients=5,
-        min_evaluate_clients=1,
+        min_evaluate_clients=5,
         min_available_clients=5,
         evaluate_fn=get_eval_fn(model, day),
         on_fit_config_fn=fit_config,
@@ -180,8 +180,8 @@ def get_eval_fn(model, day):
             with np.load(f'round-1-min.npz') as data:
                 X_min = data['X_min']
         else:
-            X_max = 1000*np.ones(40)
-            X_min = -1000*np.zeros(40)
+            X_max = 10*np.ones(37)
+            X_min = -10*np.ones(37)
         
         X_test_ben_ = scale_data(X_test_ben, X_min, X_max)
 
@@ -192,8 +192,9 @@ def get_eval_fn(model, day):
         rec_mal = dict()
         mse_mal = dict()
         for folder in list(X_test_mal.keys()):
-            rec_mal[folder] = model.predict(X_test_mal[folder])
-            mse_mal[folder] = np.mean(np.power(X_test_mal[folder] - rec_mal[folder], 2), axis=1)
+            X_test_mal_ = scale_data(X_test_mal[folder], X_min, X_max)
+            rec_mal[folder] = model.predict(X_test_mal_)
+            mse_mal[folder] = np.mean(np.power(X_test_mal_ - rec_mal[folder], 2), axis=1)
 
         # Detect all the samples which are anomalies.
         anomalies_ben = sum(mse_ben > threshold)
@@ -235,7 +236,7 @@ def fit_config(rnd: int):
     local epoch, increase to two local epochs afterwards.
     """
     config = {
-        "batch_size": 32,
+        "batch_size": 128,
         "local_epochs": 1 if rnd < 2 else 2,
     }
     return config
@@ -262,7 +263,7 @@ def evaluate_config(rnd: int):
 
     print("[*] Evaluate config with threshold:", threshold)
 
-    val_steps = 5 
+    val_steps = 10 
     # if rnd < 4 else 10
     return {
         "val_steps": val_steps,

anomaly_detection/utils.py
@@ -10,18 +10,18 @@ data_dir = '/opt/Malware-Project/BigDataset/FEELScenarios/'
 def get_model():
     model = tf.keras.Sequential(
         [
-        tf.keras.layers.Input(shape=(40)),
-        tf.keras.layers.Dense(36, activation='elu'),
-        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Input(shape=(37)),
+        tf.keras.layers.Dense(32, activation='elu'),
+        tf.keras.layers.Dropout(0.1),
         tf.keras.layers.Dense(20, activation='elu'),
-        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dropout(0.1),
         tf.keras.layers.Dense(10, activation='elu'),
-        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dropout(0.1),
         tf.keras.layers.Dense(20, activation='elu'),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.Dense(36, activation='elu'),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.Dense(40, activation='elu')
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(32, activation='elu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(37, activation='elu')
         ]
     )
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss="mse")
@@ -33,10 +33,10 @@ def get_ben_data(day: int, client_id: int):
     df_ben = pd.read_csv(os.path.join(data_dir, 'Processed', 'Client'+str(client_id), 'Day'+str(day), "comb_features_ben.csv"))
     df_ben_test = pd.read_csv(os.path.join(data_dir, 'Processed', 'Client'+str(client_id), 'Day'+str(day+1), "comb_features_ben.csv"))
 
-    df_ben = df_ben.drop(["ssl_ratio"], axis=1)
+    df_ben = df_ben.drop(["ssl_ratio", "self_signed_ratio", "ratio_certificate_path_error", "ratio_missing_cert_in_cert_path"], axis=1)
     df_ben = df_ben.drop_duplicates()
 
-    df_ben_test = df_ben_test.drop(["ssl_ratio"], axis=1)
+    df_ben_test = df_ben_test.drop(["ssl_ratio", "self_signed_ratio", "ratio_certificate_path_error", "ratio_missing_cert_in_cert_path"], axis=1)
     df_ben_test = df_ben_test.drop_duplicates()
 
     return df_ben, df_ben_test
@@ -52,15 +52,15 @@ def get_mal_data():
         mal_data[folder] = pd.concat([mal_data[folder], df_temp], ignore_index=True)
 
     for folder in mal_folders:
-        mal_data[folder] = mal_data[folder].drop(["ssl_ratio"], axis=1)
+        mal_data[folder] = mal_data[folder].drop(["ssl_ratio", "self_signed_ratio", "ratio_certificate_path_error", "ratio_missing_cert_in_cert_path"], axis=1)
         mal_data[folder] = mal_data[folder].drop_duplicates() 
 
     return mal_data
 
 def get_threshold(X, mse):
-    num = 0.015*len(X)
+    num = 0.02*len(X)
 
-    th = 0.001
+    th = 0.0001
     while (sum(mse > th) > num):
         th += 0.001
     return th
@@ -94,6 +94,6 @@ def deserialize_string(b64_str: str):
     return [float(element) for element in arr.split('|') if element != '']
 
 def scale_data(X, X_min, X_max):
-    X_std = (X - X_min) / (X_max - X_min)
+    X_std = (X - X_min) / (X_max - X_min + 1e-5)
     X_std = np.nan_to_num(X_std, 1.0)
     return X_std